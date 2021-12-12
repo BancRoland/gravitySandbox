8e9e1c8cca7c8b29c8773ae6d6678573412cb51a
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-12 23:40:11 +0100

    összeolvasztás pályaanimációvá

--- gravitySandbox.m
+++ gravitySandbox.m
@@ -4,42 +4,37 @@ close all;
 gamma=0.1;
 dt=0.1;
 
-
 masses=[10, 0.01, 0.0001];    %tömegek
-positions=[0 0 ; 1 0 ; 1 1]'; %pozíciók
-velocity=[0 0 ; 1 0 ; 1 1]';   %sebességek
+positions=[0 0 ; 1 0 ; 2 0]'; %pozíciók
+velocity=[0 0 ; 0 1 ; 0 0.8]';   %sebességek
 
 bodyCount=length(masses);
 dim=height(positions);
 
-%% vektorkülönbség mátrix (egyik égitestrő a másikba milyen vektor mutat)
+figure(2);
 
-diffMatrix=[];
+for t=0:dt:dt*1000
+    
+    diffMatrix=[];
     for incCord=1:dim
         diffMatrix(:,:,incCord)=bsxfun(@minus,positions(incCord,:),positions(incCord,:)');
     end
-
-
-%% távolság mátrix (egyik égites többitől mért távolsága)
-distMatrix=sqrt(diffMatrix(:,:,1).^2+diffMatrix(:,:,2).^2)+eye(bodyCount);%+diffMatrix(:,:,3).^2);
-normDiffMatrix=bsxfun(@rdivide,diffMatrix,distMatrix);
-massesMatrix=(ones(bodyCount)-eye(bodyCount))*sqrt((masses'*masses).*eye(bodyCount));
     
-%% gyorsulásmátrix (egyik égitest többi fele mérhető gyorsulása)
-accelMatrix=(gamma*massesMatrix./distMatrix.^2).*normDiffMatrix;
-accelVec=[accelMatrix(:,:,1)*ones(bodyCount,1),accelMatrix(:,:,2)*ones(bodyCount,1)]'
-
-
-%%  kirajzolása a kezdeti pozícióknak
-
-figure;
-xlim([-2,2]);
-ylim([-2,2]);
-hold on;
-for incBod=1:bodyCount %pozíciók kirajzolása
-    scatter(positions(1,incBod),positions(2,incBod));
-end
-
-for inc=2:bodyCount %erők kirajzolása
-    scatter(accelMatrix(1,inc,1),accelMatrix(1,inc,2),'x');
+    distMatrix=sqrt(diffMatrix(:,:,1).^2+diffMatrix(:,:,2).^2)+eye(bodyCount);%+diffMatrix(:,:,3).^2);
+    normDiffMatrix=bsxfun(@rdivide,diffMatrix,distMatrix);
+    massesMatrix=(ones(bodyCount)-eye(bodyCount))*sqrt((masses'*masses).*eye(bodyCount));
+    
+    accelMatrix=(gamma*massesMatrix./distMatrix.^2).*normDiffMatrix;
+    accelVec=[accelMatrix(:,:,1)*ones(bodyCount,1),accelMatrix(:,:,2)*ones(bodyCount,1)]'
+    velocity=velocity+accelVec*dt;
+    positions=positions+velocity*dt
+    %     clf;
+    hold on;
+    for incBod=1:bodyCount
+        scatter(positions(1,incBod),positions(2,incBod),'.','k');
+    end
+    xlim([-5,5]);
+    ylim([-5,5]);
+    axis square;
+    pause(0.001);
 end
