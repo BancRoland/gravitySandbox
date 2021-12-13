0f8c8eddcea2afa5d306bee45b9053a07bb7905e
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-13 02:11:27 +0100

    párhuzamosan ábrázolt csillag, illetve bolygó referencia

--- gravitySandbox.m
+++ gravitySandbox.m
@@ -4,7 +4,7 @@ close all;
 homePlanet=2;
 gamma=0.1;
 dt=0.1;
-T=300*dt;
+T=100*dt;
 
 masses=[10, 0, 0, 0];    %tömegek
 positions=[0 0 ; 1 0 ; 3 0 ; 0.5 0]'; %pozíciók
@@ -15,6 +15,15 @@ dim=height(positions);
 
 figure(2);
 set(gcf, 'Position', get(0, 'Screensize'));
+
+subplot(1,2,1)
+xlim([-5,5]);
+ylim([-5,5]);
+axis square;
+subplot(1,2,2)
+xlim([-5,5]);
+ylim([-5,5]);
+axis square;
 
 for t=0:dt:T
     
@@ -28,17 +37,31 @@ for t=0:dt:T
     massesMatrix=(ones(bodyCount)-eye(bodyCount))*sqrt((masses'*masses).*eye(bodyCount));
     
     accelMatrix=(gamma*massesMatrix./distMatrix.^2).*normDiffMatrix;
-    accelVec=[accelMatrix(:,:,1)*ones(bodyCount,1),accelMatrix(:,:,2)*ones(bodyCount,1)]'
+    accelVec=[accelMatrix(:,:,1)*ones(bodyCount,1),accelMatrix(:,:,2)*ones(bodyCount,1)]';
     velocity=velocity+accelVec*dt;
     positions=positions+velocity*dt;
-    %     clf;
+    
     hold on;
-    for incBod=1:bodyCount
+%     clf;
+    for incBod=3:bodyCount
+        subplot(1,2,2);
+        hold on;
+        scatter(positions(1,1)-positions(1,homePlanet),positions(2,1)-positions(2,homePlanet),'o','y','filled');
+        scatter(positions(1,2)-positions(1,homePlanet),positions(2,2)-positions(2,homePlanet),'.','b');
         scatter(positions(1,incBod)-positions(1,homePlanet),positions(2,incBod)-positions(2,homePlanet),'.','k');
-%           scatter(positions(1,incBod),positions(2,incBod),'.','k');
+        
+        subplot(1,2,1);
+        hold on;
+        scatter(positions(1,1),positions(2,1),'o','y','filled');
+        scatter(positions(1,2),positions(2,2),'.','b');
+        scatter(positions(1,incBod),positions(2,incBod),'.','k');
+        
     end
-    xlim([-5,5]);
-    ylim([-5,5]);
+    subplot(1,2,1);
+    axis([-5 5 -5 5]);
+    axis square;
+    subplot(1,2,2);
+    axis([-5 5 -5 5]);
     axis square;
     pause(0.001);
 end
