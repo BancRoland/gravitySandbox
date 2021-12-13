5a47e54371a645516264bb994e2275fd0ce23b4e
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-13 05:07:22 +0100

    Intenzitásfüggvény ábrázolva

--- gravitySandbox.m
+++ gravitySandbox.m
@@ -2,12 +2,12 @@ clear all;
 close all;
 %% állandók
 gamma=0.1;
-dt=0.1;
-T=300*dt;
+dt=0.2;
+T=200*dt;
 
-masses=[10, 0.01, 0.0001];    %tömegek
-positions=[0 0 ; 1 0 ; 2 0]'; %pozíciók
-velocity=[0 0 ; 0 1 ; 0 0.8]';   %sebességek
+masses=[10, 0.01];    %tömegek
+positions=[0 0 ; 1 0]'; %pozíciók
+velocity=[0 0 ; 0 1.2]';   %sebességek
 
 bodyCount=length(masses);
 dim=height(positions);
@@ -17,16 +17,17 @@ set(gcf,'units','normalized','outerposition',[0 0 1 1]);
 
 distGraph=[];   %leendő távolsgáfüggvény
 angleGraph=[];
+intensityGraph=[];
 t=0:dt:T;
 
-subplot(4,1,[1,3]);
-    xlim([-5,5]);
-    ylim([-5,5]);
-    axis square;
-    
- subplot(4,1,4);
-    xlim([0,T]);
-    ylim([0,1.5]);
+% subplot(4,1,[1,3]);
+% xlim([-5,5]);
+% ylim([-5,5]);
+% axis square;
+
+% subplot(4,1,4);
+% xlim([0,T]);
+% ylim([0,1.5]);
 
 for incT=1:length(t)
     
@@ -36,7 +37,7 @@ for incT=1:length(t)
     end
     
     distMatrix=sqrt(diffMatrix(:,:,1).^2+diffMatrix(:,:,2).^2)+eye(bodyCount);%+diffMatrix(:,:,3).^2);
-    normDiffMatrix=bsxfun(@rdivide,diffMatrix,distMatrix)
+    normDiffMatrix=bsxfun(@rdivide,diffMatrix,distMatrix);
     massesMatrix=(ones(bodyCount)-eye(bodyCount))*sqrt((masses'*masses).*eye(bodyCount));
     
     accelMatrix=(gamma*massesMatrix./distMatrix.^2).*normDiffMatrix;
@@ -46,27 +47,42 @@ for incT=1:length(t)
     
     distGraph=[distGraph distMatrix(1,2)];  %ábrázolni kívánt távolságfüggvény
     angleGraph=[angleGraph atan2d(diffMatrix(1,2,2),diffMatrix(1,2,1))];
+    intensityGraph=[intensityGraph 1/distMatrix(1,2)^2];
     %     clf;
     
     subplot(4,1,[1,2]);
     hold on;
+    grid on;
     for incBod=1:bodyCount
         scatter(positions(1,incBod),positions(2,incBod),'.','k');
     end
     axis([-5 5 -5 5]);
-        axis square;
-%     subplot(2,1,1);
-%     xlim([-5,5]);
-%     ylim([-5,5]);
-
-%     axis square;
-%     
+    axis square;
+    %     subplot(2,1,1);
+    %     xlim([-5,5]);
+    %     ylim([-5,5]);
+    
+    %     axis square;
+    %
+    
     subplot(4,1,3)
-    plot(t(1:incT),distGraph)
+    title('Távolság a csillagtól');
+    grid on;
+    hold on;
+    plot(t(1:incT),distGraph,'k')
     axis([0 T 0 (max(distGraph)+0.1)]);
     
     subplot(4,1,4)
-    plot(t(1:incT),angleGraph)
-    axis([0 T -180 180]);
+    title('Besugárzott teljesítmény');
+    grid on;
+    hold on;
+    plot(t(1:incT),intensityGraph,'k')
+    axis([0 T 0 (max(intensityGraph)+0.1)]);
+
+    
+    %     subplot(4,1,4)
+    %     plot(t(1:incT),angleGraph)
+    %     axis([0 T -180 180]);
+    
     pause(0.001);
 end
