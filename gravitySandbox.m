a62844324cec71e7412c9d5b91991701075ee426
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-13 01:48:41 +0100

    bolygó referenciájú égbolt

--- gravitySandbox.m
+++ gravitySandbox.m
@@ -1,19 +1,22 @@
 clear all;
 close all;
 %% állandók
+homePlanet=2;
 gamma=0.1;
 dt=0.1;
+T=300*dt;
 
-masses=[10, 0.01, 0.0001];    %tömegek
-positions=[0 0 ; 1 0 ; 2 0]'; %pozíciók
-velocity=[0 0 ; 0 1 ; 0 0.8]';   %sebességek
+masses=[10, 0, 0, 0];    %tömegek
+positions=[0 0 ; 1 0 ; 3 0 ; 0.5 0]'; %pozíciók
+velocity=[0 0 ; 0 1 ; 0 0.5; 0 1.5]';   %sebességek
 
 bodyCount=length(masses);
 dim=height(positions);
 
 figure(2);
+set(gcf, 'Position', get(0, 'Screensize'));
 
-for t=0:dt:dt*1000
+for t=0:dt:T
     
     diffMatrix=[];
     for incCord=1:dim
@@ -27,11 +30,12 @@ for t=0:dt:dt*1000
     accelMatrix=(gamma*massesMatrix./distMatrix.^2).*normDiffMatrix;
     accelVec=[accelMatrix(:,:,1)*ones(bodyCount,1),accelMatrix(:,:,2)*ones(bodyCount,1)]'
     velocity=velocity+accelVec*dt;
-    positions=positions+velocity*dt
+    positions=positions+velocity*dt;
     %     clf;
     hold on;
     for incBod=1:bodyCount
-        scatter(positions(1,incBod),positions(2,incBod),'.','k');
+        scatter(positions(1,incBod)-positions(1,homePlanet),positions(2,incBod)-positions(2,homePlanet),'.','k');
+%           scatter(positions(1,incBod),positions(2,incBod),'.','k');
     end
     xlim([-5,5]);
     ylim([-5,5]);
